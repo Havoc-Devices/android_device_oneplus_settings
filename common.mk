91045e600f3b3d230e8ddc625106b852546a3e66
Michael Bestas <mkbestas@lineageos.org>
2018-01-26 00:38:08 +0200

    oneplus-settings: Merge keyhandler back into ConfigPanel
    * Dexpreopt is now enabled globally, but we can't dexpreopt
      keyhandler jar for some reason, resulting in selinux denials
    * Merge keyhandler back into ConfigPanel as there is no real reason
      to split it and it fixes the denials as well
    
    Change-Id: Id822618221d36bf4f28384519625bf5a8f7a40d8

--- common.mk
+++ common.mk
@@ -14,15 +14,9 @@
 # limitations under the License.
 #
 
-# overlays
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Keyhandler
 PRODUCT_PACKAGES += \
-    OPSettings \
-    com.oneplus.keyhandler
-
-PRODUCT_SYSTEM_SERVER_JARS += com.oneplus.keyhandler
-
-# never dexopt the keyhandler
-$(call add-product-dex-preopt-module-config,com.oneplus.keyhandler,disable)
+    OPSettings
