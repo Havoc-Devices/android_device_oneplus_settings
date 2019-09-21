d4d0ef61b3009c8ef014611a1df92a73652c0364
Anushek Prasal <anushekprasal@gmail.com>
2019-09-21 21:47:42 +0530

    oneplus-settings: Move to product

--- opsettings/Android.mk
+++ opsettings/Android.mk
@@ -21,6 +21,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRODUCT_MODULE := true
 LOCAL_PACKAGE_NAME := OPSettings
 
 LOCAL_USE_AAPT2 := true
